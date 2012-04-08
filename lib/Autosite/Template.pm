bca1cdc6fc23840248935ac7fc6d09a96d1b9827
Gines R <ginesr@gmail.com>
2012-04-08 20:14:15 -0300

    basic render functionality

--- lib/Autosite/Template.pm
+++ lib/Autosite/Template.pm
@@ -6,26 +6,62 @@ use autodie;
 use Moose;
 use Try::Tiny;
 use Autosite::Error;
+use IO::File;
 
-has 'cache' => ( is => 'rw' );
+has 'cache' => ( is => 'rw', default => sub { return {} }, lazy => 1 );
+has 'file'  => ( is => 'rw', isa => 'Str' );
 
 sub render {
 
-	my $self        = shift;
-	my $template    = shift;    # file handle or string
-	my $namespace   = shift;
-	my $export_vars = shift;
-	my $remove_tags = shift;
-	my $output_to   = shift;    # file handle, string
-	
-	if (not $template) {
-		Autosite::Error->throw('missing template parameter');
-	}
-	if ($template and ref($template) and ref($template) ne 'IO::File' ) {
-		Autosite::Error->throw('invalid template ref');
-	}
-	
-	return 1;
+    my $self        = shift;
+    my $namespace   = shift;
+    my $export_vars = shift;
+    my $remove_tags = shift;
+    my $output_to   = shift;    # file handle, string
+
+    my $template_output = $self->get_template_contents();
+
+    if ( not $namespace ) {
+        Autosite::Error->throw('missing namespace parameter');
+    }
+    if ( $namespace and ref($namespace) ne 'HASH' ) {
+        Autosite::Error->throw('invalid ref type in namespace');
+    }
+
+    foreach ( keys %{$namespace} ) {
+        $namespace->{ uc($_) } =~ s/\$([A-Z_0-9\.]+)/$namespace->{$1}/g;
+    }
+
+    $template_output =~ s/\$([A-Z_0-9\.]+)/$namespace->{$1}/g;
+
+    return $template_output;
+
+}
+
+sub get_template_contents {
+
+    my $self = shift;
+
+    if ( not $self->file ) {
+        Autosite::Error->throw('missing file parameter');
+    }
+
+    my $template = IO::File->new();
+    $template->open( $self->file, 'r' );
+
+    if ( not $template ) {
+        Autosite::Error->throw('IO error');
+    }
+    if ( $template and ref($template) and ref($template) ne 'IO::File' ) {
+        Autosite::Error->throw('invalid template ref');
+    }
+
+    local ($/) = undef;
+    my $template_output = <$template>;
+    
+    $self->cache->{$self->file} = $template_output;
+
+    return $template_output;
 
 }
 
--- t/template.t
+++ t/template.t
@@ -1,9 +1,19 @@
 #!perl
 
+use strict;
+use warnings;
+use autodie;
 use Autosite::Template;
-use Test::More tests => 1;
+use Test::More tests => 2;
 use Test::Exception;
 
 my $template = Autosite::Template->new;
+
 throws_ok( sub { $template->render() },
- 'Autosite::Error', 'Missing parameters' );
+    'Autosite::Error', 'Missing parameters' );
+
+$template->file('templates/test1.htm');
+
+my $output = $template->render( { TEST => 'some text' } );
+
+like( $output, qr/Replace some text/, 'Replace one var' );
